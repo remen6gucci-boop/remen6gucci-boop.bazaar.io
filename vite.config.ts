e3124b1f4c4eb817693871b919f868b87da98664
remeshok gucci <remen6gucci@gmail.com>
2026-07-22 19:35:43 +0300

    fallback

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: './',
+  base: '/remen6gucci-boop.bazaar.io/',
   plugins: [react(), tailwindcss()],
   server: {
     host: '127.0.0.1',
